901b3dbbf59f732afe6293653dbae07446bd5612
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-10 13:29:00 +0530

    Working With Component Styles

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -2,7 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server', 
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    h1{
+        color: dodgerblue;
+        }
+        
+    `]
 })
 
 export class ServerComponent {
