df1051e3069d38f6e1e9030b7fd67d29ea8d6ea3
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-22 22:13:59 +0530

    Adding Content To The Recipes Components

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.telegraph.co.uk/content/dam/recipes/2020/03/13/227561993_A-Jenny-Zarins-wwwjennyzarinscom-contactjennyzarinscom-44-07958-208-486_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=450')
+  ];
   constructor() { }
 
   ngOnInit(): void {
